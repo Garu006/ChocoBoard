3807128e18ab2f50cf3f463ac356884488e443d2
Garu006 <garojas@uamv.edu.ni>
2026-01-25 13:48:00 -0600

    implementar CustomKeyboardView y soporte para tipografía externa (Chewy)

---
+++ app/src/main/java/com/chocoboard/service/CustomKeyboardView.java
@@ -0,0 +1,43 @@
+package com.chocoboard.service;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.util.AttributeSet;
+import androidx.core.content.res.ResourcesCompat;
+import com.chocoboard.R;
+import java.util.List;
+
+public class CustomKeyboardView extends KeyboardView{
+    public CustomKeyboardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        //Obtenemos la fuente personalizada "Chewy"
+        Typeface typeface = ResourcesCompat.getFont(getContext(), R.font.chewy);
+
+        // se configura el pincel para dibujar las letras
+        Paint paint = new Paint();
+        paint.setTextAlign(Paint.Align.CENTER);
+        paint.setAntiAlias(true);
+        paint.setTypeface(typeface);
+        paint.setTextSize(55); // este numero se puede ajustar si las letras se quieren mas grandes o pequeñas
+        paint.setColor(0xFF000000); // color negro
+
+        // dibujar el fondo del teclado original
+        super.onDraw(canvas);
+
+        // Dibuja las etiquetas de las teclas con la nueva fuente
+        List<Keyboard.Key> keys = getKeyboard().getKeys();
+        for (Keyboard.Key key : keys) {
+            if (key.label != null) {
+                canvas.drawText(key.label.toString(), key.x + (key.width/2), key.y + (key.height/2) + 18, paint);
+            }
+        }
+    }
+}
